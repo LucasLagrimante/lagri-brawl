5b5f5d117660c9ac8d47cba15757504a31103b64
Lucas Lagrimante <lucaslagrimante@live.com>
2024-09-11 02:50:12 -0300

    #fonte

--- src/components/MapList.js
+++ src/components/MapList.js
@@ -53,10 +53,10 @@ const MapList = () => {
                     <div className="map-card" key={map.id}>
                         <div className="map-info">
                             <img src={map.imageUrl} width={200} alt={map.name} />
-                            <h3>{map.name} ({map.gameMode.name}
+                            <h3>{map.name} (<span style={{ fontSize: '15px' }}>{map.gameMode.name}</span>
                                 <img src={map.gameMode.imageUrl} width={20} alt={map.name} />)
                             </h3>
-                            <p style={{ fontSize: '12px' }}>Atualizado em: {new Date(parseInt(map.dataUpdated) * 1000).toLocaleString('pt-BR', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false })}</p>
+                            <p style={{ fontSize: '10px' }}>Atualizado em: {new Date(parseInt(map.dataUpdated) * 1000).toLocaleString('pt-BR', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false })}</p>
                         </div>
                         <BrawlerList mapId={map.id} sortByWinRate={sortByWinRate} /> {/* Passa o estado de ordenação para BrawlerList */}
                     </div>
