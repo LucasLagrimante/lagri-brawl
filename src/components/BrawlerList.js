e4e1d42fbf69819fd358a3feb658747839cd5a0b
LucasL. <lucaslagrimante@live.com>
2024-10-14 16:29:36 -0300

    Update BrawlerList.js

--- src/components/BrawlerList.js
+++ src/components/BrawlerList.js
@@ -1,29 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { getMapDetails, getAllBrawlers } from '../services/brawlApi';
 
-const BrawlerList = ({ mapId, sortByWinRate }) => {
+const BrawlerList = ({ mapName, sortByWinRate }) => {
     const [brawlers, setBrawlers] = useState([]);
 
     useEffect(() => {
         const fetchBrawlers = async () => {
-            const mapDetails = await getMapDetails(mapId);
+            const mapDetails = await getMapDetails(mapName);
             const brawlersData = await getAllBrawlers();
 
             // Extraindo os 10 brawlers mais jogados
             const mostUsedBrawlers = mapDetails.stats
-                .filter(brawler => {
-                    // Aplica o filtro de useRate somente se o modo de jogo for diferente de Solo Showdown, Duo Showdown ou Trio Showdown
-                    const gameMode = mapDetails.gameMode.name;
-                    if (gameMode !== 'Solo Showdown' && gameMode !== 'Duo Showdown' && gameMode !== 'Trio Showdown') {
-                        return brawler.useRate >= 2.1;
-                    } else {
-                        return brawler.useRate >= 1.5;
-                    }
-                })
-                .filter(brawler => brawler.brawler && brawler.brawler !== 0) // Filtra brawlers com brawler vazio ou igual a zero
-                .filter((brawler, index, self) =>
-                    index === self.findIndex((b) => b.brawler === brawler.brawler) // Remove duplicados
-                )
                 .sort((a, b) => !sortByWinRate ? b.winRate - a.winRate : b.useRate - a.useRate) // Ordena pela taxa de uso ou taxa de vitória
                 .slice(0, 10); // Seleciona os 10 mais usados
 
@@ -48,7 +35,7 @@ const BrawlerList = ({ mapId, sortByWinRate }) => {
         };
 
         fetchBrawlers();
-    }, [mapId, sortByWinRate]); // Adicionado sortByWinRate como dependência
+    }, [mapName, sortByWinRate]); // Adicionado sortByWinRate como dependência
 
     return (
         <ul className="brawler-list">
